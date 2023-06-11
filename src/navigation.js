9e6919121a55383309d01f0e00237130418b7cd6
EGOR <staer_mag@mail.ru>
2023-06-11 20:21:12 +0300

    added moving from room to room

--- src/navigation.js
+++ src/navigation.js
@@ -3,8 +3,36 @@
 // import player from './data/player.js';
 // раскомментируй импорты при работе (закомментил, чтобы линтер не ругался)
 
+const directions = [
+  ['8', 'up', 'north'],
+  ['6', 'right', 'east'],
+  ['2', 'down', 'south'],
+  ['4', 'left', 'west'],
+  ['9', 'pageup', 'up'],
+  ['3', 'pagedown', 'down'],
+];
 const navigation = (pressedKey) => {
-  console.log(pressedKey);
+  const direction = directions.reduce((acc, [key1, key2, dir]) => {
+    if (key1 === pressedKey || key2 === pressedKey) {
+      acc = dir;
+    }
+    return acc;
+  }, undefined);
+  if (direction) {
+    console.log(direction);
+    const lastRoom = player.room;
+    const lastObj = map[lastRoom];
+    if (lastObj.exits[direction]) {
+      const nextRoom = lastObj.exits[direction];
+      player.room = nextRoom;
+      const nextObj = map[nextRoom]
+      console.log(`Вы перешли в ${nextObj.description}`);
+    } else {
+      console.log(`В направлении ${direction} нет двери`);
+    };
+  } else {
+    console.log('Данная кнопка не управляет воином');
+  }
 };
 
 export default navigation;
