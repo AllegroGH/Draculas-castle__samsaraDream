bdd2f6887343bf082639a472298c63b3264b6d27
Dmitry <8351270@gmail.com>
2023-06-21 18:20:26 +0300

    commit with map without descriptions and 1 floor to draw

--- src/help/show-map.js
+++ src/help/show-map.js
@@ -1,31 +1,57 @@
 const showMap = (map, player) => {
   var cur_room = `${player.room}`;
   var cur_floor =  `${map[player.room].floor}`;
+  var cur_floor = 1;
 //  console.log(`showMap resolve ('visited' key in this room): ${map[player.room].visited}`);
-//Our map is a matrix 9x7. Every room has number and name. 
-  var floor_1 = {6:'room101',13:'room102',14:'room103',15:'room104',
-  19:'room102', 20:'room102', 21:'room102', 22:'room102', 23:'room102', 24:'room102', 26:'room102', 27:'room102', 
-  28:'room102', 29:'room102', 31:'room102', 32:'room102', 33:'room102', 34:'room102', 35:'room102',  36:'room102',
-  41:'room102', 42:'room102', 43:'room102', 50:'room102', 51:'room102', 52:'room102', 60:'room102'}
-  var floor_2 = {6:'room101',13:'room102',14:'room103',15:'room104'} 
-  var floor_3 = {6:'room101',13:'room102',14:'room103',15:'room104'} 
+//Our map is a matrix 10x7. Every room has number and name. 
+  var floor_1 = {5:'room5',10:'room10',11:'room11',12:'room12',
+  15:'room15', 16:'room16', 17:'room17', 18:'room18', 19:'room19', 20:'room20', 21:'room21', 22:'room22', 
+  23:'room23', 24:'room24', 25:'room25', 26:'room26', 27:'room27', 28:'room28', 32:'room32',  33:'room33',
+  34:'room34', 40:'room40'}
+  var floor_2 = {47:'room47',52:'room52',53:'room53',54:'room54',57:'room57',58:'room58',59:'room59',60:'room60',
+  61:'room61',62:'room62',63:'room63',64:'room64',65:'room65',66:'room66',67:'room67',68:'room68',
+  69:'room69',70:'room70',75:'room75'} 
+  var floor_3 = {87:'room87',88:'room88',89:'room89',94:'room94', 95:'room95',96:'room96',101:'room101',102:'room102',
+  103:'room103',109:'room109'} 
 
   function get_up(i,j) {
     var visited = map[floor_1[i]].visited;
-    var north = "";
+    var left = "";
+    var right = "";
+    var center = "";
     var obj = map[floor_1[i]].exits;	  
-    ('north' in obj)?north = '│   │':north = '┌───┐';	     
-    visited?north=north:north='*****';
-    return north;  
+    center = '   ';
+    ('west' in obj)?left = '─':left = '┌';	     
+    ('east' in obj)?right = '─':right = '┐';
+    if ('north' in obj)
+    {
+      ('east' in obj)?right=' ':right = '│';
+      ('west' in obj)?left=' ':left = '│';
+    }
+    else center = '───';
+    var head = left+center+right;	  
+    visited?head=head:head='*****';
+    return head;  
   }
 
   function get_dw(i,j) {
     var visited = map[floor_1[i]].visited;
+    var left = "";
+    var right = "";
+    var center = "";
     var obj = map[floor_1[i]].exits;	  
-    var south = "";
-    ('south' in obj)?south = '│   │':south = '└───┘';
-    visited?south=south:south='*****';
-    return south;  
+    center = '   ';
+    ('west' in obj)?left = '─':left = '└';	     
+    ('east' in obj)?right = '─':right = '┘';
+    if ('south' in obj)
+    {
+      ('east' in obj)?right=' ':right = '│';
+      ('west' in obj)?left=' ':left = '│';
+    }
+    else center = '───';
+    var tail = left+center+right;	  
+    visited?tail=tail:tail='*****';
+    return tail;  
   }
 
   function get_mid(i,j) {
@@ -49,17 +75,28 @@ const showMap = (map, player) => {
   var up=""; 	 
   var mid=""; 	 
   var dw=""; 	 
-  for (let i=1; i<64; i++)
+  for (let i=1; i<43; i++)
   {
-    (i in floor_1)?up+=get_up(i,1):up+=empty;	  
-    (i in floor_1)?mid+=get_mid(i,1):mid+=empty;	  
-    (i in floor_1)?dw+=get_dw(i,1):dw+=empty;	  
-//    (i in map_matrix)?console.log(get_up(i)):console.log('damnit');
-//    console.log(up);
-//    console.log(mid);
-//    console.log(dw);
+    if (cur_floor==1)
+    {
+      (i in floor_1)?up+=get_up(i,cur_floor):up+=empty;	  
+      (i in floor_1)?mid+=get_mid(i,cur_floor):mid+=empty;	  
+      (i in floor_1)?dw+=get_dw(i,cur_floor):dw+=empty;	  
+    }
+    if (cur_floor==2)
+    {
+      (i in floor_2)?up+=get_up(i,cur_floor):up+=empty;	  
+      (i in floor_2)?mid+=get_mid(i,cur_floor):mid+=empty;	  
+      (i in floor_2)?dw+=get_dw(i,cur_floor):dw+=empty;	  
+    }
+    if (cur_floor==3)
+    {
+      (i in floor_3)?up+=get_up(i,cur_floor):up+=empty;	  
+      (i in floor_3)?mid+=get_mid(i,cur_floor):mid+=empty;	  
+      (i in floor_3)?dw+=get_dw(i,cur_floor):dw+=empty;	  
+    }
     k++;
-    if (k>8) {
+    if (k>6) {
       k=0;	    
       console.log(up);
       console.log(mid);
