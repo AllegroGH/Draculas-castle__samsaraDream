13e7f91d9e17c441ce71cb4f8db6d831051c49e1
Allegro <alex.makovka@gmail.com>
2023-06-26 19:45:55 +0300

    show-map: move const

--- src/help/show-map.js
+++ src/help/show-map.js
@@ -1,58 +1,67 @@
+const floor1 = {
+  5: 'room5',
+  10: 'room10',
+  11: 'room11',
+  12: 'room12',
+  15: 'room15',
+  16: 'room16',
+  17: 'room17',
+  18: 'room18',
+  19: 'room19',
+  20: 'room20',
+  21: 'room21',
+  22: 'room22',
+  23: 'room23',
+  24: 'room24',
+  25: 'room25',
+  26: 'room26',
+  27: 'room27',
+  28: 'room28',
+  32: 'room32',
+  33: 'room33',
+  34: 'room34',
+  40: 'room40',
+};
+const floor2 = {
+  47: 'room47',
+  52: 'room52',
+  53: 'room53',
+  54: 'room54',
+  57: 'room57',
+  58: 'room58',
+  59: 'room59',
+  60: 'room60',
+  61: 'room61',
+  62: 'room62',
+  63: 'room63',
+  64: 'room64',
+  65: 'room65',
+  66: 'room66',
+  67: 'room67',
+  68: 'room68',
+  69: 'room69',
+  70: 'room70',
+  74: 'room74',
+};
+const floor3 = {
+  87: 'room87',
+  88: 'room88',
+  89: 'room89',
+  94: 'room94',
+  95: 'room95',
+  96: 'room96',
+  101: 'room101',
+  102: 'room102',
+  103: 'room103',
+  109: 'room109',
+};
+const floors = { 1: floor1, 2: floor2, 3: floor3 };
+
 const showMap = (map, player) => {
   const curRoom = `${player.room}`;
   const curFloor = Number(`${map[player.room].floor}`);
   // var curFloor = 1;
   // Our map is a matrix 10x7. Every room has number and name.
-  const floor1 = {
-    5: 'room5',
-    10: 'room10',
-    11: 'room11',
-    12: 'room12',
-    15: 'room15',
-    16: 'room16',
-    17: 'room17',
-    18: 'room18',
-    19: 'room19',
-    20: 'room20',
-    21: 'room21',
-    22: 'room22',
-    23: 'room23',
-    24: 'room24',
-    25: 'room25',
-    26: 'room26',
-    27: 'room27',
-    28: 'room28',
-    32: 'room32',
-    33: 'room33',
-    34: 'room34',
-    40: 'room40',
-  };
-  const floor2 = {
-    47: 'room47',
-    52: 'room52',
-    53: 'room53',
-    54: 'room54',
-    57: 'room57',
-    58: 'room58',
-    59: 'room59',
-    60: 'room60',
-    61: 'room61',
-    62: 'room62',
-    63: 'room63',
-    64: 'room64',
-    65: 'room65',
-    66: 'room66',
-    67: 'room67',
-    68: 'room68',
-    69: 'room69',
-    70: 'room70',
-    74: 'room74',
-  };
-  const floor3 = {
-    87: 'room87', 88: 'room88', 89: 'room89', 94: 'room94', 95: 'room95', 96: 'room96', 101: 'room101', 102: 'room102', 103: 'room103', 109: 'room109',
-  };
-  const floors = { 1: floor1, 2: floor2, 3: floor3 };
-
   function getUp(i, j) {
     let left = '';
     let right = '';
@@ -68,7 +77,9 @@ const showMap = (map, player) => {
       left = 'west' in obj ? ' ' : '│';
     } else center = '───';
     let head = left + center + right;
-    if (!visited) { head = '░░░░░'; }
+    if (!visited) {
+      head = '░░░░░';
+    }
     return head;
   }
 
@@ -87,7 +98,9 @@ const showMap = (map, player) => {
       left = 'west' in obj ? ' ' : '│';
     } else center = '───';
     let tail = left + center + right;
-    if (!visited) { tail = '░░░░░'; }
+    if (!visited) {
+      tail = '░░░░░';
+    }
     return tail;
   }
 
@@ -102,7 +115,9 @@ const showMap = (map, player) => {
     west = 'west' in obj ? ' ' : '│';
     east = 'east' in obj ? ' ' : '│';
     room = curRoom === key ? `${west} * ${east}` : `${west}   ${east}`;
-    if (!visited) { room = '░░░░░'; }
+    if (!visited) {
+      room = '░░░░░';
+    }
     return room;
   }
 
